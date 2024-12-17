dff3666549efc8962aea2b6e5cc6ac1a26c3aa58
gregortokarev <tokarev.egor3@wildberries.work>
2024-12-17 07:44:39 +0300

    Fix `save_day.sh`

--- save_day.sh
+++ save_day.sh
@@ -1,5 +1,13 @@
 read -p "Day number:" day_number
 
+if [ -d "day${day_number}" ]; then
 mkdir "day${day_number}"
+fi
+
+if [ -f ./main.go ]; then
 mv main.go "day${day_number}/main.go"
+fi
+
+if [ -f ./data.txt ]; then
 mv data.txt "day${day_number}/day.txt"
+fi
